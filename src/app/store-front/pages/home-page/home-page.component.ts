0905261cb091baaaa8019b8f4b1618e9e92223cb
PacoAlexM <pako_ale_martell@hotmail.com>
2025-09-23 22:56:35 -0600

    Configuracion de servicio para obtener productos

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'home-page',
     imports: [ProductCardComponent],
     templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+    productsService = inject(ProductsService);
+    productsResource = rxResource({
+        params: () => ({}),
+        stream: ({ params }) => {
+            return this.productsService.getProducts();
+        },
+    });
+}

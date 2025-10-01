749f34a1776f1cc8e2426c9ec32f714c320f33cc
PacoAlexM <pako_ale_martell@hotmail.com>
2025-09-30 22:52:12 -0600

    Configuracion de pagina de producto

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ProductsResponse } from '@products/interfaces/product.interface';
+import { Product, ProductsResponse } from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Options } from '@products/interfaces/options.interface';
@@ -17,6 +17,12 @@ export class ProductsService {
         const { limit = 9, offset = 0, gender = '' } = options;
 
         return this.http.get<ProductsResponse>(`${baseUrl}/products`, { params: { limit, offset, gender, } }).pipe(
+            tap(res => console.log(res))
+        );
+    }
+
+    getProductBySlug(slug: string): Observable<Product> {
+        return this.http.get<Product>(`${baseUrl}/products/${slug}`).pipe(
             tap(res => console.log(res))
         );
     }
--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
     selector: 'product-page',
     imports: [],
     templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent {}
+export class ProductPageComponent {
+    productsService = inject(ProductsService);
+    productSlug = inject(ActivatedRoute).snapshot.params['idSlug'];
+
+    productResource = rxResource({
+        params: () => this.productSlug,
+        stream: ({ params: productSlug }) => {
+            return this.productsService.getProductBySlug(productSlug);
+        },
+    });
+}

ecc3b5efbc38891553abcf794b97cd876954905f
PacoAlexM <pako_ale_martell@hotmail.com>
2025-10-02 22:29:18 -0600

    Configuracion de paginas de genero

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal, rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductCardComponent } from "@products/components/product-card/product-card.component";
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
     selector: 'gender-page',
-    imports: [],
+    imports: [ProductCardComponent, TitleCasePipe],
     templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent {}
+export class GenderPageComponent {
+    route = inject(ActivatedRoute);
+    productsService = inject(ProductsService);
+    gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+    productsResource = rxResource({
+        params: () => this.gender(),
+        stream: ({ params: gender }) => {
+            return this.productsService.getProducts({ gender });
+        },
+    });
+}

08391404fa0edc57961f6f5f6297ebd052880dc9
PacoAlexM <pako_ale_martell@hotmail.com>
2025-10-23 22:47:14 -0600

    Se agrega tabla de productos al panel admin

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductsTableComponent } from "@products/components/products-table/products-table.component";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
     selector: 'products-admin-page',
-    imports: [ProductsTableComponent],
+    imports: [ProductsTableComponent, PaginationComponent],
     templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent {}
+export class ProductsAdminPageComponent {
+    productsService = inject(ProductsService);
+    paginationService = inject(PaginationService);
+
+    productsResource = rxResource({
+        params: () => this.paginationService.currenPage() - 1,
+        stream: ({ params: currenPage }) => {
+            return this.productsService.getProducts({ offset: currenPage * 9 });
+        },
+    });
+}
